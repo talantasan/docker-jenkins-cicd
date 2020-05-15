c84af298e9cb8e10174f87be4d9e5ede29784adb
Talant Asan <uchkunfan@gmail.com>
2020-05-15 07:27:46 -0400

    mysqlbackup code fixed with

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -6,4 +6,4 @@ DB_PASS=$3
 
 mysqldump $DB_NAME -h$DB_HOSTNAME  -uroot -p$DB_PASS > /tmp/talantsql-backup-$(date +%Y-%m-%d-%T).sql
 aws s3 cp /tmp/talantsql-backup-$(date +%Y-%m-%d-%T).sql s3://talant-sql-backup
-rm -rf /tmp/talantsql-backup-$(date +%Y-%m-%d-%T).sql
+rm -rf /tmp/talantsql-backup-*.sql
