b28f7c96b784a3abe25accd57a4ebe2ff5c98819
Talant Asan <uchkunfan@gmail.com>
2020-05-15 19:46:06 -0400

    mysql backup code has been updated

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-MY_DATE=$(date +%Y-%m-%d)
-BACKUP_FILE=mysql-backup-talant-$MY_DATE
+mysqldump talantdb  -h db_host -u root -pmysql1234  > /tmp/mysql-backup-talant.sql
 
-DB_NAME=$1
-DB_HOSTNAME=$2
-DB_PASS=$3
-
-AWS_ID=$4
-AWS_SECRET=$5
-BUCKET_NAME=$6
-
-
-mysqldump $DB_NAME -h$DB_HOSTNAME  -uroot -p$DB_PASS > /tmp/$BACKUP_FILE.sql && \
-export AWS_ACCESS_KEY_ID=$AWS_ID && \
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
-aws s3 cp /tmp/$BACKUP_FILE s3://$BUCKET_NAME
 
