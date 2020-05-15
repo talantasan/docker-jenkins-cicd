adde60d76e969d813ee4fdd8c10466d35827d5be
Talant Asan <uchkunfan@gmail.com>
2020-05-15 08:01:29 -0400

    file has been updated

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -5,5 +5,5 @@ DB_HOSTNAME=$2
 DB_PASS=$3
 
 mysqldump $DB_NAME -h$DB_HOSTNAME  -uroot -p$DB_PASS > /tmp/talantsql-backup-$(date +%Y-%m-%d-%T).sql
-aws s3 cp /tmp/talantsql-backup-$(date +%Y-%m-%d-%T).sql s3://talant-sql-backup
+aws s3 cp /tmp/talantsql-backup-*.sql s3://talant-sql-backup
 rm -rf /tmp/talantsql-backup-*.sql
