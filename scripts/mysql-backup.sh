39d68d871c5d923ee7d240a6aa9be86c88a55f98
Talant Asan <uchkunfan@gmail.com>
2020-05-15 05:28:20 -0400

    script has been updated

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -4,4 +4,4 @@ DB_NAME=$1
 DB_HOST=$2
 DB_PASSWD=$3
 
-mysqldump $DB_NAME -h$DB_HOST -uroot -p$DB_PASSWD  > tmp/talant-script-bucket-$DATE.sql
+mysqldump $DB_NAME -h$DB_HOST -uroot -p$DB_PASSWD  > /tmp/talant-script-bucket-$DATE.sql
