0554bf3290cbe560cc041a3d3568f3a211010985
Talant Asan <uchkunfan@gmail.com>
2020-05-15 07:11:34 -0400

    mysql backup has been evolved

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-DATE=$(date +'%T+%m+%Y')
-DB_NAME=$1
-DB_HOST=$2
-DB_PASSWD=$3
 
-mysqldump $DB_NAME -h$DB_HOST -uroot -p$DB_PASSWD  > /tmp/talant-script-bucket-$DATE.sql
+DB_NAME=$1
+DB_HOSTNAME=$2
+DB_PASS=$3
+
+mysqldump $DB_NAME -h$DB_HOSTNAME  -uroot -p$DB_PASS > /tmp/talantsql-backup-$(date +%Y-%m-%d-%T).sql
+aws s3 cp /tmp/talantsql-backup-$(date +%Y-%m-%d-%T).sql s3://talant-sql-backup
+rm -rf /tmp/talantsql-backup-$(date +%Y-%m-%d-%T).sql
