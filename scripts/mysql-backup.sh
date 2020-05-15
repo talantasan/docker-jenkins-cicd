5d8cf26b8f27f5054d4b8c0db04b3a85cdffb514
Talant Asan <uchkunfan@gmail.com>
2020-05-15 19:54:16 -0400

    mysql backup has been improved

--- scripts/mysql-backup.sh
+++ scripts/mysql-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mysqldump talantdb  -h db_host -u root -pmysql1234  > /tmp/mysql-backup-talant.sql
+mysqldump talantdb  -h db_host -u root -pmysql1234  > /tmp/talant-mysql-$(date +%m-%d-%y=%H-%M).sql
 
 
